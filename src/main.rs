18628aead085d8f7b04c063dda8c9839c6528791
Sagiv Marzini <sagivmarzini1@gmail.com>
2025-09-18 21:41:43 +0300

    Refactor main function: improve error handling and clean up imports

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,31 @@
-use std::{time::Instant, process::exit, env, fs};
+use std::{env, fs, time::Instant};
+
+use crate::lex::lexer::Lexer;
 
 mod lex;
 
-
 fn main() {
-
-    //get main args and check the second one (the file to compile)
+    // Get command-line arguments and check the second one (the file to compile)
     let args: Vec<String> = env::args().collect();
-
     if args.len() < 2 {
-        println!("Error: please provide a file to compile");
-        exit(1);
+        panic!("Error: please provide a file to compile");
     }
-    
-    //get the input file and read it into input
+
+    // Read the input file
     let input = fs::read_to_string(&args[1]).expect("Error reading file");
 
-    
-    let mut lexer = lex::lexer::Lexer::new(input.chars().collect());
+    let mut lexer = Lexer::new(input.chars().collect());
     println!("Tokens: \n{:?}\n", lexer.lex().unwrap());
-
 
     //let lexer = Lexer::new(&source.chars().collect());
     //record_time("tokenization", lex);
 }
 
-
-// generic function to record time it takes to run a function
+/// Record time it took to run a function
 fn record_time<F, R>(name: &str, func: F) -> R
 where
     F: FnOnce() -> R,
 {
-    println!("starting {}", name);
     let start = Instant::now();
 
     let result = func();
